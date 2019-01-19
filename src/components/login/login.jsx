3744d4b1117ed8b6e2a2286c5b219da87f120458
Javier Vega <jvega@Javiers-MacBook-Pro.local>
2019-01-19 12:39:21 -0600

    card for user page

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
-import { Jumbotron,Image} from 'react-bootstrap';
-import './login.css'
+import { Jumbotron, Image, Row, Col, Grid } from 'react-bootstrap';
+import { Card, Button,} from 'reactstrap';
+
+import './login.css';
 
 export default class User extends Component {
-render() {
-    return (
-        <header>
-        <Jumbotron>
-            <Image src = "assets/img/logo2.jpg" width ="100%"></Image>
-        </Jumbotron>
-        </header>
-    
-        );
-    }
+	render() {
+		return (
+			<header>
+				<Jumbotron>
+					<Image src="assets/img/logo2.jpg" width="100%" />
+				</Jumbotron>
+				<Grid>
+					<Row className="show-grid">
+						<Col xs={6} md={4}>
+                        <Card body className="text-center">
+								<Image src ="assets/img/letter.png" width = "150" height ="100"></Image>
+								<Button class = "text-center">Create</Button>
+							</Card>
+						</Col>
+						
+					</Row>
+				</Grid>
+			</header>
+		);
+	}
 }
