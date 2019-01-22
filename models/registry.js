19de63914253d6acbad2e1b7216280ca9bdd376d
Michael Wenhold <40506588+michaelwenhold@users.noreply.github.com>
2019-01-21 23:31:00 -0600

    sequelize for registry

---
+++ models/registry.js
@@ -0,0 +1,19 @@
+module.exports = function (sequelize, Sequelize) {
+    var tasks = sequelize.define("registry", {
+        item: {
+            type: Sequelize.STRING,
+            validate: {
+                isAlphanumeric: true,
+            }
+        },
+
+        quantity: {
+            type: Sequelize.INTEGER,
+            validate: {
+                isAlphanumeric: true,
+            }
+        },
+    });
+    
+    return tasks;
+};
