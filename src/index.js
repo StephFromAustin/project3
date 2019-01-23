ece9087d3a31001393b41d2d7931d0f0818c4f8b
Stephanie Gonzales <stephgonza@utexas.edu>
2019-01-22 18:21:56 -0600

    steph adding to devs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 let state = {};
 window.setState = (changes) => {
@@ -11,9 +10,35 @@ window.setState = (changes) => {
     ReactDOM.render(<App {...state}/>, document.getElementById('root'));
 }
 
+/* eslint no-restricted-globals: 0*/
+
 let initialState = {
     name: "amanda",
     location: location.pathname.replace(/^\/?|\/$/g, "")
 };
 
 window.setState(initialState);
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// let state = {};
+// window.setState = (changes) => {
+//     state = Object.assign({}, state, changes);
+
+//     ReactDOM.render(<App {...state}/>, document.getElementById('root'));
+// }
+
+// let initialState = {
+//     name: "amanda",
+//     location: location.pathname.replace(/^\/?|\/$/g, "")
+// };
+
+// window.setState(initialState);
