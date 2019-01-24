a22f186415b5ebb3d0a2095048989764df53c016
motoko104 <motoko104@yahoo.com>
2019-01-23 19:51:28 -0600

    updated UI from javier

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navbar/navbar';
 import Home from './components/home/home';
-import NavbarLogin from './components/navbar/navbar';
-import About from './components/about/about';
-import Login from './components/login/login';
-import What from './components/what/what';
+import ModalLogin  from './components/modal/modal';
 
 class App extends Component {
   render() {
-
     return (
       <Router>
         <div>
-          <NavbarLogin/>
-          <Route exact path ="/"component = {Home} />
-          <Route exact path = "/about" component = {About}/>
-          <Route exact path = "/login" component = {Login}/>
-          <Route exact path = "/what" component = {What}/>
+          <Navbar/>
+          <Route exact path = "/" component = {Home}/>
+          <Route exact path ="./modal" component ={ModalLogin }/>
         </div>
       </Router>
-    )
+    );
   }
 }
 
